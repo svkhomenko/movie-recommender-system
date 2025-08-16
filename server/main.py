3eefbd3d1725551e161ee7ce4cd3040ff1dd06c6
Svetlana Khomenko <hsvetlana03@gmail.com>
2025-08-16 18:00:11 +0300

    add CORS

--- server/main.py
+++ server/main.py
@@ -2,10 +2,20 @@ import uvicorn
 from dotenv import load_dotenv
 import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[os.getenv("CLIENT_URL")],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allow_headers=["Content-Type", "Authorization", "Set-Cookie"],
+    expose_headers=["X-Total-Count"],
+)
 
 
 @app.get("/")
