6ba8e290bd79c4884ab215e20d68586d91bf1ed9
Svetlana Khomenko <hsvetlana03@gmail.com>
2025-08-16 17:46:34 +0300

    add .env

--- server/main.py
+++ server/main.py
@@ -1,4 +1,9 @@
+import uvicorn
+from dotenv import load_dotenv
+import os
 from fastapi import FastAPI
+
+load_dotenv()
 
 app = FastAPI()
 
@@ -6,3 +11,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host=os.getenv("SERVER_HOST"), port=int(os.getenv("SERVER_PORT")))
