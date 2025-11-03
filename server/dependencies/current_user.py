3f8bdc8fed82c7f616ba0285e5796aded389628f
Svetlana Khomenko <hsvetlana03@gmail.com>
2025-11-03 02:10:17 +0200

    add optional auth

--- server/dependencies/current_user.py
+++ server/dependencies/current_user.py
@@ -1,7 +1,8 @@
-from fastapi import Depends, HTTPException, status
+from fastapi import Depends, HTTPException, status, Query
 from typing import Annotated
 from models.user import User
-from dependencies.oauth2 import oauth2_scheme
+from models.movie import MoviePublicResultType, MoviePublicFilterSearchParams
+from dependencies.oauth2 import oauth2_scheme, oauth2_scheme_optional
 from dependencies.session import SessionDep
 from services import token as TokenService
 
@@ -26,4 +27,25 @@ def get_current_user(
     return user
 
 
+def get_current_user_for_personal_lists(
+    token: Annotated[str | None, Depends(oauth2_scheme_optional)],
+    session: SessionDep,
+    query: Annotated[MoviePublicFilterSearchParams, Query()],
+):
+    if (
+        query.result_type == MoviePublicResultType.CONTINUE_WATCHING
+        or query.result_type == MoviePublicResultType.RECOMMENDATIONS
+    ):
+        if token:
+            return get_current_user(token, session)
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="The access token is invalid or has expired",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+
 CurrentUserDep = Annotated[User, Depends(get_current_user)]
+CurrentUserForPersonalListsDep = Annotated[
+    User | None, Depends(get_current_user_for_personal_lists)
+]
--- server/dependencies/oauth2.py
+++ server/dependencies/oauth2.py
@@ -3,5 +3,6 @@ from typing import Annotated
 from fastapi import Depends
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+oauth2_scheme_optional = OAuth2PasswordBearer(tokenUrl="/auth/login", auto_error=False)
 
 OAuth2Dep = Annotated[OAuth2PasswordRequestForm, Depends()]
