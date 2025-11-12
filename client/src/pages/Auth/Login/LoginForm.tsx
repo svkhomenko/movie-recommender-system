1dc4483f10b3ebd0758ad9ba89ee899529613078
Svetlana Khomenko <hsvetlana03@gmail.com>
2025-11-12 23:22:54 +0200

    remove reset when login error

--- client/src/pages/Auth/Login/LoginForm.tsx
+++ client/src/pages/Auth/Login/LoginForm.tsx
@@ -23,7 +23,6 @@ const LoginForm = () => {
     register,
     handleSubmit,
     formState: { errors },
-    reset,
   } = useForm<ILogin>({
     resolver: zodResolver(loginSchema),
   });
@@ -33,7 +32,6 @@ const LoginForm = () => {
     form.append('username', username);
     form.append('password', password);
     await loginHandler(form);
-    reset();
   });
 
   return (
